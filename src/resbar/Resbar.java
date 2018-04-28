20b354715dd1a3c0c8006763e81f25c129cf3955
mateo <fc13023@ues.edu.sv>
2018-04-28 00:08:12 -0600

    Primer commit

---
+++ src/resbar/Resbar.java
@@ -0,0 +1,14 @@
+
+package resbar;
+
+/**
+ *
+ * @author mateo
+ */
+public class Resbar {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
